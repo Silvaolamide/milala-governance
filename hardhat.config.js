434678033ad06d998a10d340f0e2cc2ba1d03ea0
Olamide Agunkejoye <silvaolamide@gmail.com>
2022-11-18 19:45:01 +0100

    udated-env

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,8 +4,11 @@ require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-etherscan");
 require("hardhat-contract-sizer");
 require("dotenv").config();
-
-
+const { mnemonic, ETHERSCAN_KEY } = require('./.secret.json');
+// console.log(process.env)
+// const DEV_PRIVATE_KEY = process.env.DEV_PRIVATE_KEY;
+// const  ETHERSCAN_KEY =  process.env.ETHERSCAN_KEY
+// console.log(DEV_PRIVATE_KEY)
 module.exports = {
   solidity: {
     version: "0.8.9",
@@ -19,14 +22,11 @@ module.exports = {
   networks: {
     testnet3: {
       url: `https://data-seed-prebsc-1-s1.binance.org:8545/`,
-      accounts: ["8d93db770ffda91bce9e758fb88c6bb6ff1adb6191f125481844d6fa4c64d589"],
+      accounts: [mnemonic]
     },
   },
   etherscan: {
-    // apiKey: "QY163355TNRJXJFY9WFM8WAHNBS6FF63HN",
-    // apiKey: "PNTIGJ3SRJU7SQ5FRXHUIT94DN9BC3WCMK",
-    apiKey: {
-      bscTestnet: "PNTIGJ3SRJU7SQ5FRXHUIT94DN9BC3WCMK"
-  }
+    apiKey: ETHERSCAN_KEY
+  
   },
 };
